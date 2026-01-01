ab4f3e381a75ffd5503d2b173b6f8b5b54eda6a6
drpoudel3785 <dharmarajpoudel@hotmail.com>
2026-01-01 09:04:49 +0545

    operator in c

---
+++ operators.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int num1, num2, sum;
+    num1=10;
+    num2=20;
+    sum=num1+num2;
+    printf("The sum of %d and %d is %d", num1, num2, sum);
+}
